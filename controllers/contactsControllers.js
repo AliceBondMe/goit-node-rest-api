ee97b061897f6fd67a2b3d126ba24261eaabf650
AliceBondMe <ellie.bond.ff@gmail.com>
2024-02-03 18:22:53 +0200

    Update contactsControllers.js

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -3,28 +3,32 @@ import checkId from "../helpers/checkId.js";
 import Contact from "../models/contact.js";
 
 export const getAllContacts = async (req, res) => {
-  const { page = 1, limit = 5, favorite } = req.query;
-  const { _id: owner } = req.user;
-  const skip = (page - 1) * limit;
+  try {
+    const { page = 1, limit = 5, favorite } = req.query;
+    const { _id: owner } = req.user;
+    const skip = (page - 1) * limit;
 
-  let query = { owner };
+    let query = { owner };
 
-  if (favorite !== undefined) {
-    query.favorite = favorite === "true";
+    if (favorite !== undefined) {
+      query.favorite = favorite === "true";
+    }
+
+    const totalContacts = await Contact.countDocuments(query);
+
+    const contacts = await Contact.find(query, "-createdAt -updatedAt", {
+      skip,
+      limit,
+    }).populate("owner", "email");
+    res.status(200).json({
+      total: totalContacts,
+      perPage: limit,
+      currentPage: page,
+      contacts: contacts,
+    });
+  } catch (error) {
+    next(error);
   }
-
-  const totalContacts = await Contact.countDocuments(query);
-
-  const contacts = await Contact.find(query, "-createdAt -updatedAt", {
-    skip,
-    limit,
-  }).populate("owner", "email");
-  res.status(200).json({
-    total: totalContacts,
-    perPage: limit,
-    currentPage: page,
-    contacts: contacts,
-  });
 };
 
 export const getOneContact = async (req, res, next) => {
@@ -51,10 +55,13 @@ export const deleteContact = async (req, res, next) => {
 
     const { _id: owner } = req.user;
 
-    const contact = await Contact.findById(id).where("owner").equals(owner);
-    if (!contact) throw HttpError(404);
+    const contact = await Contact.findByIdAndDelete(id)
+      .where("owner")
+      .equals(owner);
 
-    await Contact.findByIdAndDelete(id);
+    if (!contact) {
+      throw HttpError(404);
+    }
 
     res.status(200).json(contact);
   } catch (error) {
